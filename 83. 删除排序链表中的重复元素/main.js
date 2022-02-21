dad3f87d26b6ae6c7cf309704f36a3d1746db4e4
Teddypedia <1009626007@qq.com>
2022-02-21 22:30:54 +0800

    优化代码

--- 83. 删除排序链表中的重复元素/main.js
+++ 83. 删除排序链表中的重复元素/main.js
@@ -9,4 +9,20 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function (head) {}
+var deleteDuplicates = function (head) {
+    const set = new Set()
+    let pre,
+        cur = head
+    while (cur) {
+        if (set.has(cur.val)) {
+            // 删除该节点，由于只有一个节点时不用删除，所以pre必然不为空
+            pre.next = cur.next
+            cur = cur.next
+        } else {
+            set.add(cur.val)
+            pre = cur
+            cur = cur.next
+        }
+    }
+    return head
+}
